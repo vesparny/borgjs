484c07ee0cf1ee673532ad7bf39bfa0d0cd6ed5f
Alessandro Arnodo <alessandro@arnodo.net>
2017-04-18 18:06:41 +0200

    oko

--- test/fixtures/config.js
+++ test/fixtures/config.js
@@ -1,8 +1,9 @@
+const path = require('path')
+
 module.exports = {
-  repository: '',
-  paths: [ // MANDATORY
-    //  '/Users/me',
-    //  '/etc
+  repository: path.resolve(__dirname, 'test.repo'),
+  paths: [
+    path.resolve(__dirname, 'data')
   ],
   create: {
     options: [
@@ -10,53 +11,24 @@ module.exports = {
       '--filter', 'AME?'
     ]
   },
-   check: {
-   options: []
-   },
-
-  // Retention policy for pruning old backups
-  // Use the options array to pass any options supported by borg prune
-  // https://borgbackup.readthedocs.org/en/stable/usage.html#borg-prune for details.
- //  prune: {
- //   options: [
- //    '-d', 30,
- //    '-w', 30,
- //    '--keep-within', '31d'
- //   ]
- // }
-
-  // Set the following environment variables
-  // See https://borgbackup.readthedocs.io/en/stable/usage.html#environment-variables
-  env: {
-    BORG_REMOTE_PATH: 'borg1',
-    BORG_PASSPHRASE: 'passphrase'
+  check: {
+    options: []
   },
-
-  // Email configuration
-  // See https://github.com/nodemailer/nodemailer options
-  //  email: {
-  //    from: 'me@site.net',
-  //    to: 'you@site.net',
-  //    smtpHost: 'smtp.mailgun.org',
-  //    smtpPort: 587,
-  //    user: 'postmaster@lalala.mailgun.org',
-  //    pass: 'mypass',
-  //    secure: false
-  //  },
-
-  // Send an email on success (the 'email' section must be configured)
-
-  // sendSuccessMail: true,
-
-  // Send an email on error (the 'email' section must be configured)
-  // sendErrorMail: true,
-
-  // Send native Desktop notifications on success/error
-  sendSystemNotification: true
-
-  // Send push notifications on success/error to your devices connected to pushbullet
-  // See https://www.pushbullet.com/
-  // pushbullet: {
-  //  accessToken: 'your pushbullet access token'
-  // }
+  prune: {
+    options: [
+      '--keep-within', '1d'
+    ]
+  },
+  env: {
+    // BORG_REMOTE_PATH: 'borg1',
+    BORG_PASSPHRASE: 'test'
+  },
+  onFinish: function (err, data, done) {
+    if (err) {
+      console.log('An error happened')
+    } else {
+      console.log(`Archive ${data.archiveName} created.`)
+    }
+    done()
+  }
 }
